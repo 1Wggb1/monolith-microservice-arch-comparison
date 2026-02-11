775db86df123a8e4e46e5a6fc3c117492a00fc4f
1Wggb1 <willianggbatista@gmail.com>
2026-02-11 11:36:22 -0300

    adding informations about each url; improving scripts

--- load-test/microservice/k6-test-script-create.js
+++ load-test/microservice/k6-test-script-create.js
@@ -1,32 +1,26 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
+const requests = [
+    ['POST', 'http://microservice-order:8082/orders', JSON.stringify({ productIds: [100, 200, 200], userId: 1, createAt: Date.now(), total: 200.99 })],
+    ['POST', 'http://microservice-product:8083/products', JSON.stringify({ name: "PC", description: "Computer", createAt: Date.now(), price: 99.98 })],
+];
+
 export const options = {
   stages: [
     { duration: '10s', target: 200 },
-    { duration: '20s', target: 500 },
+    { duration: '5s', target: 500 },
+    { duration: '10s', target: 500 },
     { duration: '5s', target: 0 },
   ],
+  thresholds: Object.fromEntries(
+    ['http_req_duration', 'http_reqs', 'http_req_failed']
+       .flatMap(metric => requests.map(request => [ `${metric}{url:${request[1]}}`, []]))),
 };
-
-const requests = [
-    {
-        method: 'POST',
-        url: 'http://microservice-product:8083/products',
-        body: JSON.stringify({ name: "PC", description: "Computer", createAt: Date.now(), price: 99.98 }),
-        params: { headers: { 'Content-Type': 'application/json' } }
-    },
-    {
-        method: 'POST',
-        url: 'http://microservice-order:8082/orders',
-        body: JSON.stringify({ productIds: [100, 200, 200], userId: 1, createAt: Date.now(), total: 200.99 }),
-        params: { headers: { 'Content-Type': 'application/json' } }
-    },
-];
 
 export default function() {
   const responses = http.batch(requests);
-  check(responses[0], { "status is 200": (res) => res.status === 201 });
-  check(responses[1], { "status is 200": (res) => res.status === 201 });
+  check(responses[0], { "status is 201": (res) => res.status === 201 });
+  check(responses[1], { "status is 201": (res) => res.status === 201 });
   sleep(1);
 }
--- load-test/monolith/k6-test-script.js
+++ load-test/monolith/k6-test-script.js
@@ -1,18 +1,22 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-export const options = {
-  stages: [
-    { duration: '10s', target: 200 },
-    { duration: '20s', target: 500 },
-    { duration: '5s', target: 0 },
-  ],
-};
-
 const requests = [
     ['GET', 'http://monolith-order-product:8084/orders'],
     ['GET', 'http://monolith-order-product:8084/products'],
 ];
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 200 },
+    { duration: '5s', target: 500 },
+    { duration: '10s', target: 500 },
+    { duration: '5s', target: 0 },
+  ],
+  thresholds: Object.fromEntries(
+      ['http_req_duration', 'http_reqs', 'http_req_failed']
+        .flatMap(metric => requests.map(request => [ `${metric}{url:${request[1]}}`, []]))),
+};
 
 export default function() {
   const responses = http.batch(requests);
